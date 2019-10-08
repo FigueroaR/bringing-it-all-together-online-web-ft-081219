b1e563453297f5b52307998a9c358c26e874d55a
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-10-08 22:37:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,12 +44,10 @@ class Dog
   def self.new_from_db(row)
     dog = { 
       :id => row[0], 
-    :name => row[1], 
-    :type => row[2]
-    }
-    
+      :name => row[1], 
+      :type => row[2]
+        }
     self.new(dog)
-    
     #binding.pry 
   end 
 end 
