7dc50bf8bb48147602f6285925c9cabd7188c63f
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-10-08 22:45:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ class Dog
   
   def self.find_by_id(id)
     #binding.pry
-    DB[conn:].execute("SELECT * FROM dogs WHERE id = ? ",  id).map do |row|
+    DB[:conn].execute("SELECT * FROM dogs WHERE id = ? ",  id).map do |row|
       self.new_from_db(row)
     end.first
   end 
