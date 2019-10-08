b149583075a93932f9628a0b337a0e79a5723f6c
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-10-08 19:20:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,12 +10,14 @@ class Dog
   end 
   
   def self.create_table
-    sql = "CREATE TABLE IF NOT EXISTS dogs(
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    breed TEXT)"
-    
-    DB[:conn).execute(sql)
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end 
   
 end 
